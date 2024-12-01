86df2dfe3db65525808ff23849d1de8d10b68e10
maesierra <mariaeugenia.sierra@gmail.com>
2024-12-01 21:11:38 +0000

    Day 1
    Arggg. I should read the problem.
    I wasted hours because I thought I had to do a distinct. So I used sorted sets when it should have been a priority queue
    I also wanted to use stream and reduces but in the end I stayed classic.
    
    The second part was just building the histogram.
    
    I added a nice scaffolding to hide the horrible regexp in java and do input + regexp -> stream of groups

---
+++ src/main/java/net/maesierra/adventOfCode2024/solutions/day1/Day1.java
@@ -0,0 +1,67 @@
+package net.maesierra.adventOfCode2024.solutions.day1;
+
+import net.maesierra.adventOfCode2024.Main;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.regex.Pattern;
+
+import static net.maesierra.adventOfCode2024.utils.IOHelper.inputAsStream;
+
+public class Day1 implements Main.Solution {
+
+    public static final Pattern LINE_REGEXP = Pattern.compile("(\\d+) +(\\d+)");
+
+    record Pair<T>(T left, T right) {}
+
+    @Override
+    public String part1(InputStream input, String... params) {
+        Pair<PriorityQueue<Integer>> lists = inputAsStream(input, LINE_REGEXP).reduce(
+                new Pair<>(new PriorityQueue<>(), new PriorityQueue<>()),
+                (l, groups) -> {
+                    l.left().add(Integer.valueOf(groups[0]));
+                    l.right().add(Integer.valueOf(groups[1]));
+                    return l;
+                },
+                (a, b) -> a
+
+        );
+
+        System.out.println("Left total: %d, Right total: %d".formatted(lists.left().size(), lists.right().size()));
+        int sum = 0;
+        while (!lists.left().isEmpty() && !lists.right().isEmpty()) {
+            int minLeft = lists.left().poll();
+            int minRight = lists.right().poll();
+            System.out.println("Comparing %d %d".formatted(minLeft, minRight));
+            sum += Math.abs(minLeft - minRight);
+        }
+
+        return Integer.toString(sum);
+    }
+
+    @Override
+    public String part2(InputStream input, String... params) {
+        record Lists(List<Integer> left, Map<Integer, Integer> histogram) {}
+        Lists lists = inputAsStream(input, LINE_REGEXP).reduce(
+                new Lists(new ArrayList<>(), new HashMap<>()),
+                (p, groups) -> {
+                    Integer leftValue = Integer.valueOf(groups[0]);
+                    Integer rightValue = Integer.valueOf(groups[1]);
+                    p.left().add(leftValue);
+                    p.histogram().put(
+                            rightValue,
+                            p.histogram().getOrDefault(rightValue, 0) + 1
+                    );
+                    return p;
+                },
+                (a, b) -> a
+        );
+        Integer sum = lists.left().stream()
+                .reduce(0, (s, n) -> s + (n * lists.histogram().getOrDefault(n, 0)));
+        return Integer.toString(sum);
+    }
+}
--- src/main/java/net/maesierra/adventOfCode2024/utils/IOHelper.java
+++ src/main/java/net/maesierra/adventOfCode2024/utils/IOHelper.java
@@ -5,6 +5,9 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class IOHelper {
 
@@ -15,4 +18,26 @@ public class IOHelper {
             throw new RuntimeException(e);
         }
     }
+
+    public static Stream<String> inputAsStream(InputStream input) {
+        return IOUtils.readLines(input, StandardCharsets.UTF_8).stream();
+    }
+
+    public static Stream<String[]> inputAsStream(InputStream input, Pattern regExp) {
+        return  inputAsStream(input)
+                .map(s -> {
+                    Matcher m = regExp.matcher(s);
+                    if (m.matches()) {
+                        String[] groups = new String[m.groupCount()];
+                        for (int i = 1; i <= m.groupCount(); i++) {
+                            groups[i - 1] = m.group(i);
+                        }
+                        return groups;
+                    }
+                    return new String[]{};
+                })
+                .filter(groups -> groups.length > 0);
+
+    }
+
 }
