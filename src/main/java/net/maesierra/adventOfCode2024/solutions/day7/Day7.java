831913ec9e0d57ed99271f062c9de63af9e8ebec
maesierra <mariaeugenia.sierra@gmail.com>
2024-12-08 00:01:21 +0000

    Day 7: Fun with equations
    A nice and fun day... I still remember how to solve equations
    Once I figure out I needed to the recursion from right to left it was dead easy.
    Part 2 did not work initially due to a bug, so I actually tried to do the classic queue solution calculating the value left to right... But while I was writing it, I spotted the bug.

---
+++ src/main/java/net/maesierra/adventOfCode2024/solutions/day7/Day7.java
@@ -0,0 +1,93 @@
+package net.maesierra.adventOfCode2024.solutions.day7;
+
+import net.maesierra.adventOfCode2024.Main;
+import net.maesierra.adventOfCode2024.utils.Logger;
+
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import static net.maesierra.adventOfCode2024.utils.IOHelper.inputAsStream;
+import static net.maesierra.adventOfCode2024.utils.IOHelper.inputAsString;
+
+public class Day7 implements Main.Solution {
+
+
+    record Equation(long result, List<Long> numbers) {
+
+            boolean canBeSolved(boolean useConcatenation) {
+                if (numbers.size() == 1) {
+                    return result == numbers.get(0);
+                }
+                if (numbers.size() == 2) {
+                    Long n1 = numbers.get(0);
+                    Long n2 = numbers.get(1);
+                    return result == n1 + n2 ||
+                           result == n1 * n2 ||
+                            (useConcatenation && Long.toString(result).equals("%d%d".formatted(n1, n2))) ;
+                }
+                long last = numbers.get(numbers.size() - 1);
+                List<Long> rest = numbers().subList(0, numbers().size() - 1);
+                if (result - last > 0) {
+                    if (new Equation(result - last, rest).canBeSolved(useConcatenation)) {
+                        return true;
+                    }
+                }
+                if (result % last == 0) {
+                    if (new Equation(result / last, rest).canBeSolved(useConcatenation)) {
+                        return true;
+                    }
+                }
+                if (useConcatenation) {
+                    long nDigits = BigInteger.TEN.pow(Long.toString(last).length()).longValue();
+                    if ((result - last) % nDigits  == 0) {
+                        return new Equation((result - last) / nDigits, rest).canBeSolved(true);
+                    }
+                }
+                return false;
+            }
+
+
+        @Override
+        public String toString() {
+            return "%s: %s".formatted(result, numbers());
+        }
+    }
+
+    private static Stream<Equation> parseInput(InputStream input) {
+        return inputAsStream(input, Pattern.compile("(\\d+): (.*)")).map(groups -> new Equation(
+                Long.parseLong(groups[0]),
+                Stream.of(groups[1].split(" ")).map(Long::parseLong).toList()));
+    }
+
+    @Override
+    public String part1(InputStream input, String... params) {
+        long res = parseInput(input)
+                .filter(equation -> {
+                    boolean canBeSolved = equation.canBeSolved(false);
+                    Logger.debug("%s => %s", equation, canBeSolved);
+                    return canBeSolved;
+                })
+                .mapToLong(Equation::result)
+                .sum();
+        return Long.toString(res);
+    }
+
+    @Override
+    public String part2(InputStream input, String... params) {
+        long res = parseInput(input)
+                .filter(equation -> {
+                    boolean canBeSolved = equation.canBeSolved(true);
+                    Logger.debug("%s => %s", equation, canBeSolved);
+                    return canBeSolved;
+                })
+                .mapToLong(Equation::result)
+                .sum();
+        return Long.toString(res);
+    }
+}
