78a8064991f9580fab842122cee5523dc7400600
Maria-Eugenia Sierra <mariaeugeniap@spotify.com>
2024-12-26 19:06:23 +0000

    Day 14: Part 2
    For once I decided to investigate a little bit before trying to brute force things...
    Googling for how a binary addition is implemented via logic gates I found this https://www.101computing.net/binary-additions-using-logic-gates/
    And upon closer inspection it was clear that indeed the input was clearly matching that.
    After many iterations I managed to figure out a consistent way to get the full-adder for each digit and a test function to determine if a full-adder was right. With that I could swap wires (each full-adder is only 5, so it's reasonable), re-test until I found something that works

--- src/main/java/net/maesierra/adventOfCode2024/solutions/day24/Day24.java
+++ src/main/java/net/maesierra/adventOfCode2024/solutions/day24/Day24.java
@@ -1,20 +1,22 @@
 package net.maesierra.adventOfCode2024.solutions.day24;
 
 import net.maesierra.adventOfCode2024.Runner;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.InputStream;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static net.maesierra.adventOfCode2024.utils.IOHelper.inputAsString;
 import static net.maesierra.adventOfCode2024.utils.IOHelper.inputAsTextBlocks;
+import static org.apache.commons.lang3.StringUtils.defaultString;
 
 public class Day24 implements Runner.Solution {
 
 
-    static class Wire {
+    public static class Wire {
         private Optional<Integer> value;
         private final String label;
 
@@ -48,6 +50,10 @@ public class Day24 implements Runner.Solution {
         public String toString() {
             return label;
         }
+
+        public void reset() {
+            this.value = Optional.empty();
+        }
     }
 
     static abstract class LogicalGate  {
@@ -65,6 +71,10 @@ public class Day24 implements Runner.Solution {
         protected int run(int i1, int i2) {
             return i1 & i2;
         }
+        @Override
+        public String toString() {
+            return "AND";
+        }
     }
 
     static class Or extends LogicalGate {
@@ -72,6 +82,11 @@ public class Day24 implements Runner.Solution {
         @Override
         protected int run(int i1, int i2) {
             return i1 | i2;
+        }
+
+        @Override
+        public String toString() {
+            return "OR";
         }
     }
 
@@ -81,12 +96,25 @@ public class Day24 implements Runner.Solution {
         protected int run(int i1, int i2) {
             return i1 ^ i2;
         }
+
+        @Override
+        public String toString() {
+            return "XOR";
+        }
     }
 
-    record Connections(Wire input1, Wire input2, Wire output, LogicalGate gate) {
+    public record Connections(Wire input1, Wire input2, Wire output, LogicalGate gate) {
 
+        Connections withClonedWires() {
+            return new Connections(new Wire(input1.label), new Wire(input2.label), new Wire(output.label), gate);
+        }
+
+        @Override
+        public String toString() {
+            return "%s %s %s -> %s".formatted(input1.label, gate, input2.label, output.label);
+        }
     }
-    static class Circuit {
+    public static class Circuit {
         final Map<String, Wire> wires;
         final Map<String, Wire> externalOutput;
         final Map<String, Connections> inputConnections;
@@ -96,18 +124,44 @@ public class Day24 implements Runner.Solution {
             this.wires = wires;
             this.externalOutput = wires.values().stream().filter(wire -> wire.label.startsWith("z")).collect(Collectors.toMap(Wire::label, Function.identity()));
             this.inputConnections = connections;
-            connections.values().forEach(c -> {
+            calculateConnectedTo();
+        }
+
+        private void calculateConnectedTo() {
+            connectedTo.clear();
+            this.inputConnections.values().forEach(c -> {
                 connectedTo.computeIfAbsent(c.input1.label, k -> new HashSet<>()).add(c.output);
                 connectedTo.computeIfAbsent(c.input2.label, k -> new HashSet<>()).add(c.output);
             });
         }
 
+        public void swap(String wire1, String wire2) {
+            Connections c1 = inputConnections.get(wire1);
+            Connections c2 = inputConnections.get(wire2);
+            inputConnections.remove(wire1);
+            inputConnections.remove(wire2);
+            inputConnections.put(c2.output.label, new Connections(c1.input1,c1.input2,c2.output,c1.gate));
+            inputConnections.put(c1.output.label, new Connections(c2.input1,c2.input2,c1.output,c2.gate));
+            calculateConnectedTo();
+        }
+
+        void reset() {
+            wires.values().forEach(Wire::reset);
+        }
         long run(Map<String, Integer> initialValues) {
+            return run(initialValues, Long.MAX_VALUE);
+        }
+
+        long run(Map<String, Integer> initialValues, long limit) {
             initialValues.forEach((label, value) -> wires.get(label).value = Optional.of(value));
             Deque<Wire> toProcess = wires.values().stream()
                     .filter(w -> w.value.isEmpty())
                     .collect(Collectors.toCollection(LinkedList::new));
+            long counter = 0;
             while (!toProcess.isEmpty()) {
+                if (counter > limit) {
+                    throw new RuntimeException("Limit exceeded");
+                }
                 Wire current = toProcess.pop();
                 //We do nothing if the value is already set
                 if (current.value.isEmpty()) {
@@ -118,6 +172,7 @@ public class Day24 implements Runner.Solution {
                     //If there is still no value -> back to the queue
                     if (current.value.isEmpty()) {
                         toProcess.addLast(current);
+                        counter++;
                     }
                 }
             }
@@ -125,6 +180,11 @@ public class Day24 implements Runner.Solution {
                     .sorted(Entry.<String, Wire>comparingByKey().reversed())
                     .map(e -> Integer.toString(e.getValue().value.orElseThrow())).collect(Collectors.joining());
             return Long.parseLong(output, 2);
+        }
+
+        @Override
+        public String toString() {
+            return inputConnections.values().stream().map(Connections::toString).sorted().collect(Collectors.joining("\n"));
         }
     }
 
@@ -134,8 +194,14 @@ public class Day24 implements Runner.Solution {
             String[] parts = s.split(":");
             return new Wire(parts[0], Integer.parseInt(parts[1].trim()));
         }).collect(Collectors.toMap(Wire::label, Function.identity())));
+        Stream<String> block = blocks[1];
+        Map<String, Connections> connections = parseConnections(block, wires);
+        return new Circuit(wires, connections);
+    }
+
+    static Map<String, Connections> parseConnections(Stream<String> block, Map<String, Wire> wires) {
         Map<String, Connections> connections = new HashMap<>();
-        blocks[1].forEach(s -> {
+        block.forEach(s -> {
             if (s.trim().isBlank()) {
                 return;
             }
@@ -151,7 +217,7 @@ public class Day24 implements Runner.Solution {
             };
             connections.put(output.label, new Connections(input1, input2, output, gate));
         });
-        return new Circuit(wires, connections);
+        return connections;
     }
 
     @Override
@@ -160,57 +226,172 @@ public class Day24 implements Runner.Solution {
         return Long.toString(circuit.run(Map.of()));
     }
 
-    /**
-     * Check if a, b and c are a half adder
-     *         a XOR b -> sum
-     *         a AND b -> c1
-     *
-     * @return string label for the wire that contains the carry over
-     */
-    static String halfAdder(Circuit circuit, String a, String b, String sum) {
-        // a XOR b -> sum
-        Connections xor = circuit.inputConnections.get(sum);
-        assert xor.gate instanceof Xor;
-        assert xor.input1.label.equals(a) || xor.input1.label.equals(b);
-        assert xor.input2.label.equals(a) || xor.input2.label.equals(b);
-        //a and b -> c1
-        Set<Wire> fromA = circuit.connectedTo.get(a);
-        assert fromA.size() == 2;
-        Set<Wire> fromB = circuit.connectedTo.get(b);
-        assert fromB.size() == 2;
-        Wire carryOver = fromA.stream().filter(w -> !w.label.equals(sum)).findFirst().orElseThrow();
-        assert fromB.contains(carryOver);
-        Connections and = circuit.inputConnections.get(carryOver.label);
-        assert and.gate instanceof And;
-        assert and.input1.label.equals(a) || and.input1.label.equals(b);
-        assert and.input2.label.equals(a) || and.input2.label.equals(b);
-        return carryOver.label;
 
+    public static String  label(String part, int bit) {
+        return "%s%02d".formatted(part, bit);
     }
+
+    record FullAdder(Circuit circuit, String a, String b, String cIn, String sum, String cOut) {
+        boolean test() {
+            Map<Map<String, Integer>, Map<String, Integer>> table = Map.of(
+                    Map.of(a, 0, b, 0, cIn, 0), Map.of(sum, 0, cOut, 0),
+                    Map.of(a, 0, b, 1, cIn, 0), Map.of(sum, 1, cOut, 0),
+                    Map.of(a, 1, b, 0, cIn, 0), Map.of(sum, 1, cOut, 0),
+                    Map.of(a, 1, b, 1, cIn, 0), Map.of(sum, 0, cOut, 1),
+                    Map.of(a, 0, b, 0, cIn, 1), Map.of(sum, 1, cOut, 0),
+                    Map.of(a, 0, b, 1, cIn, 1), Map.of(sum, 0, cOut, 1),
+                    Map.of(a, 1, b, 0, cIn, 1), Map.of(sum, 0, cOut, 1),
+                    Map.of(a, 1, b, 1, cIn, 1), Map.of(sum, 1, cOut, 1)
+            );
+            for (var entry:table.entrySet()) {
+                Map<String, Integer> in = entry.getKey();
+                Map<String, Integer> out = entry.getValue();
+                circuit.reset();
+                //This simple circuit should not take that long
+                try {
+                    circuit.run(in, 100);
+                } catch (Exception e) {
+                    return false;
+                }
+                Map<String, Integer> actual = circuit.wires.values().stream().filter(w -> out.containsKey(w.label)).collect(Collectors.toMap(Wire::label, w -> w.value.orElseThrow()));
+                if (!out.equals(actual)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static boolean testFullAdder(Circuit circuit, String a, String b, String cIn, String sum, String cOut) {
+        Map<Map<String, Integer>, Map<String, Integer>> table = Map.of(
+                Map.of(a, 0, b, 0, cIn, 0), Map.of(sum, 0, cOut, 0),
+                Map.of(a, 0, b, 1, cIn, 0), Map.of(sum, 1, cOut, 0),
+                Map.of(a, 1, b, 0, cIn, 0), Map.of(sum, 1, cOut, 0),
+                Map.of(a, 1, b, 1, cIn, 0), Map.of(sum, 0, cOut, 1),
+                Map.of(a, 0, b, 0, cIn, 1), Map.of(sum, 1, cOut, 0),
+                Map.of(a, 0, b, 1, cIn, 1), Map.of(sum, 0, cOut, 1),
+                Map.of(a, 1, b, 0, cIn, 1), Map.of(sum, 0, cOut, 1),
+                Map.of(a, 1, b, 1, cIn, 1), Map.of(sum, 1, cOut, 1)
+        );
+        for (var entry:table.entrySet()) {
+            Map<String, Integer> in = entry.getKey();
+            Map<String, Integer> out = entry.getValue();
+            circuit.reset();
+            circuit.run(in);
+            Map<String, Integer> actual = circuit.wires.values().stream().filter(w -> out.containsKey(w.label)).collect(Collectors.toMap(Wire::label, w -> w.value.orElseThrow()));
+            if (!out.equals(actual)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    List<String> calculateSwap(FullAdder fullAdder) {
+        Set<Set<String>> swaps = new HashSet<>();
+        List<Wire> outputWires = fullAdder.circuit.inputConnections.values().stream().map(Connections::output).toList();
+        for (var w1:outputWires) {
+            for (var w2:outputWires) {
+                if (w1 != w2) {
+                    swaps.add(Set.of(w1.label, w2.label));
+                }
+            }
+        }
+        for (var swap:swaps.stream().map(s -> s.stream().toList()).toList()) {
+            fullAdder.circuit.swap(swap.get(0), swap.get(1));
+            if (fullAdder.test()) {
+                return swap;
+            }
+            //Undo the swap
+            fullAdder.circuit.swap(swap.get(0), swap.get(1));
+        }
+        throw new RuntimeException("No swap found!!");
+    }
+
 
     @Override
     public String part2(InputStream input, String... params) {
         Circuit circuit = parseCircuit(input);
-        //Full Binary Addition
-        // x001/y001 -> Half adder -> z001/c1
-        // x002/y002/c1 -> Full adder -> z002/c2
-        // ...
-        // x???/y???/c? -> Full adder -> z???
-
-        // Half adder
-        // x001 XOR y001 -> z001
-        // x001 AND y001 -> c1
-
-        //Full adder
-        // x002 XOR y002 -> tmp1
-        // x002 AND y002 -> tmp2
-        // c1 XOR tmp1 -> z002
-        // c1 AND tmp1 -> tmp3
-        // tmp2 OR tmp3 -> c2
-
-        String carryOver = halfAdder(circuit, "x00", "y00", "z00");
+        Map<Integer, List<String>> swaps = new HashMap<>();
+        //x00,y00,z00 is a half-adder
+        //we cannot get 44 because the overflow is z45
+        for (int i = 1; i <= 43; i++) {
+            FullAdder fullAdder = getFullAdder(i, circuit);
+            if (!fullAdder.test()) {
+                List<String> swap = calculateSwap(fullAdder);
+                swaps.put(i, swap);
+                circuit.swap(swap.get(0), swap.get(1));
+                System.out.printf("Invalid full-adder at position %d -> swapping %s%n", i, swap);
+                //Back to the beginning
+                i = 0;
+            }
+        }
+        return swaps.values().stream().flatMap(List::stream).sorted().collect(Collectors.joining(","));
+    }
 
 
-        return inputAsString(input).toLowerCase();
+    private FullAdder getFullAdder(int i, Circuit circuit) {
+        Map<String, Connections> connections = new HashMap<>();
+        String a = label("x", i);
+        String b = label("y", i);
+        String sum = label("z", i);
+        for (var w1: circuit.connectedTo.get(a)) {
+            var conn = circuit.inputConnections.get(w1.label).withClonedWires();
+            connections.put(w1.label, conn);
+            if (circuit.connectedTo.get(w1.label) != null) {
+                for (var w2: circuit.connectedTo.get(w1.label)) {
+                    conn = circuit.inputConnections.get(w2.label).withClonedWires();
+                    connections.put(w2.label, conn);
+                }
+            }
+        }
+        var conn = circuit.inputConnections.get(sum).withClonedWires();
+        connections.put(sum, conn);
+        boolean invalidCIn = false;
+        if (connections.size() == 4) {
+            //This means one of the connections from a to b is connected directly to sum
+            conn = connections.values().stream()
+                    .filter(c-> !c.input1.label.equals(a) && !c.input2.label.equals(a))
+                    .filter(c -> !c.output.label.equals(sum))
+                    .findFirst()
+                    .orElseThrow();
+            Wire wire = circuit.connectedTo.get(conn.output.label).iterator().next();
+            connections.put(wire.label, circuit.inputConnections.get(wire.label));
+            invalidCIn = true;
+        }
+        Map<String, Wire> wires = new HashMap<>();
+        connections.forEach((label, c) -> {
+            wires.putIfAbsent(c.input1.label, c.input1);
+            wires.putIfAbsent(c.input2.label, c.input2);
+            wires.putIfAbsent(c.output.label, c.output);
+        });
+        connections = connections.entrySet().stream().collect(Collectors.toMap(
+                Entry::getKey,
+                e -> new Connections(
+                        wires.get(e.getValue().input1.label),
+                        wires.get(e.getValue().input2.label),
+                        wires.get(e.getValue().output.label),
+                        e.getValue().gate())
+        ));
+        //Cout must be connected to next z
+        conn = circuit.inputConnections.get(label("z", i + 1));
+        String cOut = wires.containsKey(conn.input1.label) ? conn.input1.label : conn.input2.label;
+        if (!wires.containsKey(cOut)) {
+            //In this case is safer to assume cOut is the OR gate result
+            Connections orGate = connections.values().stream().filter(c -> c.gate instanceof Or).findFirst().orElseThrow();
+            cOut = orGate.output.label;
+        }
+        String cIn = null;
+        if (!invalidCIn) {
+            //Cin must be connected to z but all its input must not be in the small circuit
+            conn = circuit.inputConnections.get(label("z", i));
+            cIn = !wires.containsKey(circuit.inputConnections.get(conn.input1.label).input1.label) ? conn.input1.label : conn.input2.label;
+        }
+        if (invalidCIn || (connections.containsKey(defaultString(cIn)))){
+            //In this case we need to lookup for the previous fullAdder and use its OR gate
+            FullAdder prev = getFullAdder(i - 1, circuit);
+            Connections orGate = prev.circuit.inputConnections.values().stream().filter(c -> c.gate instanceof Or).findFirst().orElseThrow();
+            cIn = orGate.output.label;
+        }
+        return new FullAdder(new Circuit(wires, connections), a, b, cIn, sum, cOut);
     }
 }
--- src/test/java/net/maesierra/adventOfCode2024/solutions/day24/Day24Test.java
+++ src/test/java/net/maesierra/adventOfCode2024/solutions/day24/Day24Test.java
@@ -1,7 +1,16 @@
 package net.maesierra.adventOfCode2024.solutions.day24;
 
+import net.maesierra.adventOfCode2024.solutions.day24.Day24.*;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static net.maesierra.adventOfCode2024.solutions.day24.Day24.parseConnections;
 import static net.maesierra.adventOfCode2024.testUtils.TestHelper.part1;
 import static net.maesierra.adventOfCode2024.testUtils.TestHelper.part2;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,14 +19,25 @@ import static org.hamcrest.Matchers.equalTo;
 class Day24Test {
 
     @Test
+    void testFullAdder() {
+        String diagram = """
+y22 AND x22 -> hwq
+x22 XOR y22 -> cdf
+cdf AND cmn -> knw
+cmn XOR cdf -> z22
+knw OR hwq -> fjs
+""";
+        HashMap<String, Wire> wires = new HashMap<>();
+        var connections = parseConnections(Stream.of(diagram.split("\n")), wires
+        );
+        Circuit circuit = new Circuit(wires, connections);
+        assertThat(Day24.testFullAdder(circuit, "x22", "y22", "cmn", "z22", "fjs"), equalTo(true));
+    }
+
+    @Test
     void  testPart1() {
         assertThat(part1(new Day24(), "input_24_1"), equalTo("4"));
         assertThat(part1(new Day24(), "input_24_2"), equalTo("2024"));
     }
 
-    @Test
-    void  testPart2() {
-        String expected = "09:30, 4h 20min, 13:50, 46.00€";
-        assertThat(part2(new Day24(), "input_24_1"), equalTo(expected));
-    }
 }
